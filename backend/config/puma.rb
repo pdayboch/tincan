f6582e751ae73b017a21d664aedb4dc37556fdde
Philip Dayboch <philip245@gmail.com>
2024-06-19 17:40:28 -0400

    Update Puma server configuration
    Updates the puma server configuration to run on https in production,
    but run on http in development. This is deemed safe as development
    will only be accessed locally where as production will open ports
    outside of the container to the internet.

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -13,22 +13,23 @@ threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
+  ssl_bind '0.0.0.0', '3000', {
+    key: '/etc/ssl/private/server.key',
+    cert: '/etc/ssl/certs/server.crt',
+    verify_mode: 'none'
+  }
   require "concurrent-ruby"
   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
+else
+  port ENV.fetch("PORT") { 3000 }
 end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 60 if ENV.fetch("RAILS_ENV", "development") == "development"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
