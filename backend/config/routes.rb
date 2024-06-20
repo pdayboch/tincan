762558ebbc190f304ae945aa83d8993bd4d7af49
Philip Dayboch <philip245@gmail.com>
2024-06-19 20:14:02 -0400

    Add User model
    Adds a User model which will be used to login, but also tie transactions
    to a user through an account. Account isn't created yet but will be coming
    in a future commit.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, only: [:index, :create, :update, :destroy]
   resources :subcategories, only: [:create, :update, :destroy]
+  resources :users
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
