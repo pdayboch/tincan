2e924f3d7aba168ea8b4967f115578825337578b
Philip Dayboch <philip245@gmail.com>
2024-07-02 22:27:02 -0400

    Add Category model
    Adds a category model, uniqueness constraint & relevant tests.
    Also adds the actual categories to the db/seeds.rb file. This
    will allow us to easily populate the default categories in
    devel and prod.

---
+++ backend/app/models/category.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Category < ApplicationRecord
+  # Validates that the name is unique
+  validates :name, uniqueness: { message: 'already exists' }
+end
---
+++ backend/test/models/category_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "test_helper"
+
+class CategoryTest < ActiveSupport::TestCase
+  test 'should not save duplicate category names' do
+    Category.create(name: 'UniqueName')
+    duplicate_category = Category.new(name: 'UniqueName')
+
+    assert_not duplicate_category.valid?, 'Duplicate category should not be valid'
+    assert_includes duplicate_category.errors[:name], 'already exists'
+  end
+end
