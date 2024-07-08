4d7d0c41a8fb0a706f30fa3b1e1e713b193123de
Philip Dayboch <philip245@gmail.com>
2024-07-08 11:35:05 -0400

    Configure CORs to allow frontend fetches
    Configures CORs to allow the frontend to fetch requests from the
    backend.

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'https://127.0.0.1:3000',
+    'https://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete],
+      credentials: false
+  end
+end
