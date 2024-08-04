73c7e983bac057b1db01134834ef253c9c49a96e
Philip Dayboch <philip245@gmail.com>
2024-08-04 15:31:17 -0400

    Add Schwab Checking statement parser
    This commit adds the parser class for Schwab Checking statements.
    This one was very tricky because some transaction descriptions can
    span multiple lines. The debit, credit belance columns are based on
    whitespace. The amounts don't use negative numbers to signal credits
    or debits, you need to determine which column the value is in.
    
    It is also possible that whitespace counts overlap between debits
    and credits, so we can't rely on whitespace counts to determine
    which column the amount is under. Therefore, we use keywords to
    determine if an amount is a debit or credit.
    
    Precautions were taken to raise an error if a transaction matches
    keywords from the debit and credit list, as well as if it doesn't
    match any of the keywords.
    
    Additionally, the statement date range varies based on if the
    statement is contained within a single month, or if the days span
    multiple months.
    
    I feel strongly that I wrote test cases to cover all conditions, and
    now that they are passing, I believe this parser is ready.

---
+++ backend/app/services/statement_parser/schwab_checking.rb
@@ -0,0 +1,221 @@
+module StatementParser
+  class SchwabChecking < Base
+    ALL_MONTHS = "January|February|March|April|May|June|July|August|September|October|November|December"
+    DEBIT_KEYWORDS = ["withdrawal", "funds transfer to", "debit", "check"]
+    CREDIT_KEYWORDS = ["deposit", "funds transfer from", "credit", "interest paid", "atm fee rebate", "money transfer visa direct"]
+    INFORMATIONAL_KEYWORDS = ["beginning balance", "ending balance"]
+
+    def statement_end_date
+      @statement_end_date ||= begin
+          regex = %r{
+          # Single month format
+          # July 1-30, 2023
+          Statement\ Period.*\b
+          (#{ALL_MONTHS})
+          \s+
+          \d{1,2}-(\d{1,2}), # dont capture start day, capture end day
+          \s+
+          (\d{4}) # capture year
+          |
+          # Multi month format
+          # June 30, 2023 to July 29, 2023
+          Statement\ Period.*\b
+          (?:#{ALL_MONTHS}) # dont capture start month
+          \s+
+          \d{1,2},\s+\d{4} # dont capture start day or year
+          \s+to\s+.*\b
+          (#{ALL_MONTHS}) # capture end month
+          \s+
+          (\d{1,2}),\s+(\d{4}) # capture end day and year
+          }mx
+
+          match = @text.match(regex)
+          raise("Could not extract statement end date for #{@file_path}") unless match
+
+          parse_statement_date(match)
+        end
+    end
+
+    def statement_start_date
+      @statement_start_date ||= begin
+          regex = %r{
+          # Single month format
+          # July 1-30, 2023
+          Statement\ Period.*\b
+          (#{ALL_MONTHS})
+          \s+
+          (\d{1,2})-\d{1,2}, # capture start day, dont capture end day
+          \s+
+          (\d{4}) # capture year
+          |
+          # Multi month format
+          # June 30, 2023 to July 29, 2023
+          Statement\ Period.*\b
+          (#{ALL_MONTHS}) # capture start month
+          \s+
+          (\d{1,2}),\s+(\d{4}) # capture start day and year
+          \s+to\s+.*\b
+          (?:#{ALL_MONTHS}) # dont capture end month
+          \s+
+          \d{1,2},\s+\d{4} # dont capture end day or year
+          }mx
+
+          match = @text.match(regex)
+          raise("Could not extract statement start date for #{@file_path}") unless match
+
+          parse_statement_date(match)
+        end
+    end
+
+    def statement_balance
+      @statement_balance ||= begin
+          regex = /Ending Balance\s+\$([\d,]+\.\d{2})/
+
+          match = @text.match(regex)
+          raise("Could not extract statement balance for #{@file_path}") unless match
+          match[1].gsub(",", "").to_f
+        end
+    end
+
+    def transactions
+      activity_text = extract_activity_section
+      transaction_lines = split_transaction_lines(activity_text)
+      transactions = parse_transaction_lines(transaction_lines)
+      transactions.reject { |t| t[:amount] == 0.0 }
+    end
+
+    private
+
+    def extract_activity_section
+      start_regex = /Activity/
+      end_regex = /\d{2}\/\d{2}\s+Ending Balance\s+\$[\d,]+\.\d{2}/
+
+      start_index = @text.index(start_regex)
+      end_index = @text.index(end_regex) - 1
+
+      raise("Could not find activity section in #{@file_path}") unless start_index && end_index
+
+      @text[start_index..end_index]
+    end
+
+    def split_transaction_lines(text)
+      text
+        .split("\n")
+        .map(&:strip)
+        .reject(&:empty?)
+    end
+
+    def parse_transaction_lines(lines)
+      transactions = []
+      current_transaction = nil
+      pause_parsing = true
+      lines.each do |line|
+        # Resume parsing if line begins with date (mm/dd)
+        pause_parsing = false if line.match(/^\d{2}\/\d{2}/)
+        # Pause parsing if end of page
+        pause_parsing = true if line.match(/Page \d+ of \d+/)
+        # Otherwise, leave pause_parsing as its previous value.
+
+        if pause_parsing
+          if current_transaction
+            # Finish writing current transaction if one is in progress.
+            transactions << current_transaction
+            current_transaction = nil
+          end
+          next
+        end
+
+        if line.match(/^\d{2}\/\d{2}/)
+          # new transaction
+          transactions << current_transaction if current_transaction
+          current_transaction = parse_transaction_line(line)
+        else
+          # continuation of previous transaction description
+          current_transaction[:description] += " #{line.strip}" if current_transaction
+        end
+      end # end of lines
+
+      transactions << current_transaction if current_transaction
+      transactions
+    end
+
+    def parse_transaction_line(line)
+      regex = %r{
+        (\d{2}/\d{2})\s{4} # date
+        (.+?)\s{2}         # description (non-greedy)
+        (\s+.+)            # remainder (debit, credit, balance)
+      }x
+
+      matches = line.match(regex)
+      date = parse_transaction_date(matches[1])
+      description = matches[2]
+      remainder = matches[3]
+      amount = 0.0
+
+      amount_regex = %r{
+        \s+                  # all the whitespaces
+        \$([\d,]+\.\d{2})    # the debit or credit amount
+        \s+\$[\d,]+\.\d{2}   # remaining whitespaces and balance.
+      }x
+
+      is_credit = CREDIT_KEYWORDS.any? { |kw| description.downcase.include?(kw) }
+      is_debit = DEBIT_KEYWORDS.any? { |kw| description.downcase.include?(kw) }
+
+      if is_credit && is_debit
+        raise("description matches both credit and debit keywords for #{line} in #{@file_path}")
+      end
+
+      if is_credit
+        matches = remainder.match(amount_regex)
+        raise("couldn't extract credit amount from #{line} in #{@file_path}") if matches.nil?
+        amount = format_amount(matches[1])
+      elsif is_debit
+        matches = remainder.match(amount_regex)
+        raise("couldn't extract debit amount from #{line} in #{@file_path}") if matches.nil?
+        amount = -format_amount(matches[1])
+      elsif INFORMATIONAL_KEYWORDS.any? { |kw| description.downcase.include?(kw) }
+        amount = 0.0
+      else
+        raise("unknown transaction type for #{line} in #{@file_path}")
+      end
+
+      { date: date,
+        description: description,
+        amount: amount }
+    end
+
+    def parse_transaction_date(date_str)
+      month, day = date_str.split("/").map(&:to_i)
+      year = statement_end_date.year
+
+      # If the transaction month is greater than the statement end month,
+      # it indicates the transaction happened in the previous year.
+      if month > statement_end_date.month
+        year -= 1
+      end
+
+      Date.new(year, month, day)
+    end
+
+    def parse_statement_date(match)
+      date_format = "%B %d %Y"
+
+      if match[1] # single month format
+        month = match[1]
+        day = match[2]
+        year = match[3]
+      else # multi month format
+        month = match[4]
+        day = match[5]
+        year = match[6]
+      end
+
+      Date.strptime("#{month} #{day} #{year}", date_format)
+    end
+
+    def format_amount(amount)
+      # Remove the dollar sign and commas, then convert to float
+      amount.gsub(/[,$]/, "").to_f.round(2)
+    end
+  end
+end
---
+++ backend/test/services/statement_parser/schwab_checking_test.rb
@@ -0,0 +1,289 @@
+require "test_helper"
+require "timecop"
+
+module StatementParser
+  class SchwabCheckingTest < ActiveSupport::TestCase
+    def setup
+      @file_path = "path/to/statement.pdf"
+    end
+
+    def test_statement_end_date_single_month
+      mock_text = <<-TEXT
+      Account Number        Statement Period
+      FNAME LNAME           1234567890134         November 1-30, 2023
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_equal Date.new(2023, 11, 30), parser.statement_end_date
+    end
+
+    def test_statement_end_date_multi_month
+      mock_text = <<-TEXT
+      Statement Period
+      Account Number          December 30, 2023 to
+      FNAME LNAME             123456789012      January 31, 2024
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_equal Date.new(2024, 1, 31), parser.statement_end_date
+    end
+
+    def test_statement_end_date_raises_error
+      mock_text = "Invalid text"
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_raises(RuntimeError, "Could not extract statement end date for #{@file_path}") do
+        parser.statement_end_date
+      end
+    end
+
+    def test_statement_start_date_single_month
+      mock_text = <<-TEXT
+      Account Number        Statement Period
+      FNAME LNAME           1234567890134         November 1-30, 2023
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_equal Date.new(2023, 11, 1), parser.statement_start_date
+    end
+
+    def test_statement_start_date_multi_month
+      mock_text = <<-TEXT
+      Statement Period
+      Account Number          December 30, 2023 to
+      FNAME LNAME             123456789012      January 31, 2024
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_equal Date.new(2023, 12, 30), parser.statement_start_date
+    end
+
+    def test_statement_start_date_raises_error
+      mock_text = "Invalid text"
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_raises(RuntimeError, "Could not extract statement start date for #{@file_path}") do
+        parser.statement_start_date
+      end
+    end
+
+    def test_statement_balance
+      mock_text = <<-TEXT
+      11/02    Electronic Withdrawal              $123.45                                        $1,234.56
+      CAPITAL ONE CRCARDPMT 1212
+      11/30    ATM Fee Rebate                                            $2.75                   $1,234.56
+      11/30    Interest Paid                                             $1.23                   $1,234.56
+      11/30    Ending Balance                                                                    $1,234.56
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_equal 1234.56, parser.statement_balance
+    end
+
+    def test_statement_balance_raises_error
+      mock_text = "Invalid text"
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      assert_raises(RuntimeError, "Could not extract statement balance for #{@file_path}") do
+        parser.statement_balance
+      end
+    end
+
+    def test_transactions
+      mock_text = <<-TEXT
+      Account Number        Statement Period
+      FNAME LNAME           1234567890134         November 1-30, 2023
+      Activity
+      Date
+      Posted Description                                                                      Debits                     Credits                   Balance
+      11/01    Beginning Balance                                                                                                                 $10,000.50
+      11/02    Electronic Withdrawal                                                         $300.00                                             $9,700.50
+      APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW
+      11/03    Electronic Withdrawal                                                        $1,200.00                                            $8,500.50
+      PAYPAL INST XFER 123456
+      Page 3 of 8
+      © 2023 Charles Schwab Bank, SSB. All rights reserved. Member FDIC.
+      Account Number               Statement Period
+      FNAME LNAME                                     1234567890134                 November 1-30, 2023
+      TM
+      Schwab Bank Investor Checking             (continued)                                                          Account Number: 1234567890134
+      Activity (continued)
+      Date
+      Posted Description                                                                      Debits                     Credits                   Balance
+      11/13    Electronic Deposit                                                                                      $2,132.17                 $10,632.67
+      Company Paycheck IN DIRECT DEP 123456
+      11/19    Funds Transfer to Brokerage -2123                                            $5,000.00                                            $5,632.67
+      11/30    ATM Fee Rebate                                                                                               $3.50                   $5,636.17
+      11/30    Interest Paid                                                                                                $2.54                   $5,638.71
+      11/30    Ending Balance                                                                                                                    $5,638.71
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      transactions = parser.transactions
+      assert_equal 6, transactions.size
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 2),
+        description: "Electronic Withdrawal APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW",
+        amount: -300.00,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 3),
+        description: "Electronic Withdrawal PAYPAL INST XFER 123456",
+        amount: -1200.00,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 13),
+        description: "Electronic Deposit Company Paycheck IN DIRECT DEP 123456",
+        amount: 2132.17,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 19),
+        description: "Funds Transfer to Brokerage -2123",
+        amount: -5000.00,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 30),
+        description: "ATM Fee Rebate",
+        amount: 3.50,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 11, 30),
+        description: "Interest Paid",
+        amount: 2.54,
+      }
+    end
+
+    def test_multi_month_statement_transactions
+      mock_text = <<-TEXT
+      Statement Period
+      Account Number                 September 30, 2023 to
+      FNAME LNAME                          123456789012           October 31, 2023
+      Activity
+      Date
+      Posted Description                                                                      Debits                     Credits                   Balance
+      09/30    Beginning Balance                                                                                                                 $10,000.50
+      09/30    Electronic Withdrawal                                                         $300.00                                             $9,700.50
+      APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW
+      10/13    Electronic Deposit                                                                                      $2,132.17                 $11,832.67
+      Company Paycheck IN DIRECT DEP 123456
+      10/31    ATM Fee Rebate                                                                                               $3.50                   $11,836.17
+      10/31    Interest Paid                                                                                                $2.54                   $11,838.71
+      10/31    Ending Balance                                                                                                                    $11,838.71
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      transactions = parser.transactions
+      assert_equal 4, transactions.size
+
+      assert_includes transactions, {
+        date: Date.new(2023, 9, 30),
+        description: "Electronic Withdrawal APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW",
+        amount: -300.00,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 10, 13),
+        description: "Electronic Deposit Company Paycheck IN DIRECT DEP 123456",
+        amount: 2132.17,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 10, 31),
+        description: "ATM Fee Rebate",
+        amount: 3.50,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2023, 10, 31),
+        description: "Interest Paid",
+        amount: 2.54,
+      }
+    end
+
+    def test_multi_year_statement_transactions
+      mock_text = <<-TEXT
+      Statement Period
+      Account Number                 December 31, 2023 to
+      FNAME LNAME                          123456789012           January 31, 2024
+      Activity
+      Date
+      Posted Description                                                                      Debits                     Credits                   Balance
+      12/31    Beginning Balance                                                                                                                 $10,000.50
+      12/31    Electronic Withdrawal                                                         $300.00                                             $9,700.50
+      APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW
+      01/02    Electronic Deposit                                                                                      $2,132.17                 $11,832.67
+      Company Paycheck IN DIRECT DEP 123456
+      01/31    Interest Paid                                                                                                $2.54                   $11,838.71
+      01/31    Ending Balance                                                                                                                    $11,838.71
+      TEXT
+      SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+      parser = SchwabChecking.new(@file_path)
+
+      transactions = parser.transactions
+      assert_equal 3, transactions.size
+
+      assert_includes transactions, {
+        date: Date.new(2023, 12, 31),
+        description: "Electronic Withdrawal APPLECARD GSBANK PAYMENT 123456~ Tran: ACHDW",
+        amount: -300.00,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2024, 1, 2),
+        description: "Electronic Deposit Company Paycheck IN DIRECT DEP 123456",
+        amount: 2132.17,
+      }
+
+      assert_includes transactions, {
+        date: Date.new(2024, 1, 31),
+        description: "Interest Paid",
+        amount: 2.54,
+      }
+    end
+
+    def test_leap_day_transaction
+      mock_text = <<-TEXT
+      Statement Period
+      Account Number                 February 1, 2024 to
+      FNAME LNAME                          123456789012           March 1, 2024
+      Activity
+      Date
+      Posted Description                                                                      Debits                     Credits                   Balance
+      02/01    Beginning Balance                                                                                                                 $10,000.50
+      02/29    Electronic Withdrawal                                                         $300.00                                             $9,700.50
+      LEAP DAY PAYMENT
+      03/01    Ending Balance                                                                                                                    $11,838.71
+      TEXT
+
+      Timecop.travel(Date.new(2025, 10, 1)) do
+        SchwabChecking.any_instance.stubs(:get_statement_text).returns(mock_text)
+        parser = SchwabChecking.new(@file_path)
+        transactions = parser.transactions
+
+        assert_equal 1, transactions.size
+
+        assert_includes transactions, {
+          date: Date.new(2024, 2, 29),
+          description: "Electronic Withdrawal LEAP DAY PAYMENT",
+          amount: -300.00,
+        }
+      end
+    end
+  end
+end
