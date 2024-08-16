f6084db0de8dc36caf733b51c84072564c315927
Philip Dayboch <philip245@gmail.com>
2024-08-15 22:11:06 -0400

    Fix parseStatementJob tests
    There was a bug in the parseStatementJob test where the ordering
    of the execution would cause the "should process statements and
    transactions" test to fail if it wasn't executed first. This was
    happening because the ENV variable was being reset inbetween each
    test which didn't actually work. So when the parser actually needed
    to retreive the correct ROOT_DOC_DIR env variable, it was set to the
    first value that was set during the first test. Only the "should
    process statements and transactions" test makes it through the
    entire parser, so if it was incorrectly set for the other tests, it
    didn't matter.
    
    To fix it, we instead use a class variable and keep the root
    directory the same for all tests, and only change the account directory
    between each test.

--- backend/test/jobs/parse_statements_job_test.rb
+++ backend/test/jobs/parse_statements_job_test.rb
@@ -23,42 +23,48 @@ class ParseStatementsJobTest < ActiveJob::TestCase
   end
 
   setup do
-    @root_doc_dir = Dir.mktmpdir
-    ENV["ROOT_DOC_DIR"] = @root_doc_dir
+    unless defined?(@@root_doc_dir)
+      @@root_doc_dir = Dir.mktmpdir
+      ENV["ROOT_DOC_DIR"] = @@root_doc_dir.strip
+    end
+
+    @statement_directory = "test_account"
 
     @user = User.create!(
       name: "Test User",
       email: "test@example.com",
       password: "password",
     )
-    @account = @user.accounts.create!(
-      name: "Test Account",
-      statement_directory: "test_account",
-      parser_class: "MockStatementParser",
-    )
 
-    @statement_directory = File.join(@root_doc_dir, @account.statement_directory)
+    @account_directory = File.join(@@root_doc_dir, @statement_directory).strip
     FileUtils.mkdir_p(@statement_directory)
 
-    @year_directory = File.join(@statement_directory, "2023")
+    @year_directory = File.join(@account_directory, "2023").strip
     FileUtils.mkdir_p(@year_directory)
 
-    @pdf_file_path = File.join(@year_directory, "statement.pdf")
+    @pdf_file_path = File.join(@year_directory, "statement.pdf").strip
     File.write(@pdf_file_path, "PDF content")
   end
 
   teardown do
-    FileUtils.remove_entry(@root_doc_dir)
+    FileUtils.rm_rf(@account_directory)
   end
 
   test "should process statements and transactions" do
+    account = @user.accounts.create!(
+      bank_name: "Bank",
+      name: "Test Account",
+      statement_directory: @statement_directory,
+      parser_class: "MockStatementParser",
+    )
+
     assert_difference "Statement.count", 1 do
       assert_difference "Transaction.count", 2 do
         ParseStatementsJob.perform_now
       end
     end
 
-    statement = @account.statements.last
+    statement = account.statements.last
     assert_equal Date.new(2023, 1, 31), statement.statement_date
     assert_equal 1234.56, statement.statement_balance
     assert_equal @pdf_file_path, statement.pdf_file_path
@@ -76,7 +82,11 @@ class ParseStatementsJobTest < ActiveJob::TestCase
   end
 
   test "should skip account if statement_directory is blank" do
-    @account.update(statement_directory: "")
+    @user.accounts.create!(
+      bank_name: "Bank",
+      name: "Account without statement_dir",
+      parser_class: "MockStatementParser",
+    )
 
     assert_no_difference "Statement.count" do
       assert_no_difference "Transaction.count" do
@@ -86,7 +96,11 @@ class ParseStatementsJobTest < ActiveJob::TestCase
   end
 
   test "should skip account if parser_class is blank" do
-    @account.update(parser_class: "")
+    @user.accounts.create!(
+      bank_name: "Bank",
+      name: "Account without parser class",
+      statement_directory: @statement_directory,
+    )
 
     assert_no_difference "Statement.count" do
       assert_no_difference "Transaction.count" do
@@ -96,7 +110,12 @@ class ParseStatementsJobTest < ActiveJob::TestCase
   end
 
   test "should skip if statement directory is missing" do
-    FileUtils.remove_entry(@statement_directory)
+    @user.accounts.create!(
+      bank_name: "Bank",
+      name: "Account but missing account directory",
+      statement_directory: "foo",
+      parser_class: "MockStatementParser",
+    )
 
     assert_no_difference "Statement.count" do
       assert_no_difference "Transaction.count" do
@@ -106,7 +125,13 @@ class ParseStatementsJobTest < ActiveJob::TestCase
   end
 
   test "should skip processed files" do
-    @account.statements.create!(
+    account = @user.accounts.create!(
+      bank_name: "Bank",
+      name: "Account with processed statements",
+      statement_directory: @statement_directory,
+      parser_class: "MockStatementParser",
+    )
+    account.statements.create!(
       statement_date: Date.new(2023, 1, 31),
       statement_balance: 1234.56,
       pdf_file_path: @pdf_file_path,
