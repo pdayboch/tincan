3e5236ab9ed95d1b2536bbf30726fdef8cd2ce27
Philip Dayboch <philip245@gmail.com>
2024-09-15 10:25:45 -0400

    Add simplecov for coverage reports
    This adds the simplecov gem for test coverage reports.
    
    However, it doesn't work when tests are parallelized. Therefore, we
    disable the parallelization since the tests take less than 2
    seconds to run in series. We can revisit making simplecov work with
    parallelization if and when the tests take much longer.
    
    This also installs the ostruct gem since it will no longer be
    included in the standard library in a upcoming version of Ruby
    (3.5.0).

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -1,17 +1,25 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
-require "minitest/mock"
-require "mocha/minitest"
+ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/test/' # Exclude the test directory from coverage
+  add_filter '/config/'
+  add_filter '/app/channels/'
+  add_filter '/app/mailers/'
+end
+
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/mock'
+require 'mocha/minitest'
 
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # 2024-09-15 Disable temporarily parallelize since it breaks simplecov reports.
+    # parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
