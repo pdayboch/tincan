bb8f0ed31b4b7a07e4ad522641a7226688eafe2e
Philip Dayboch <philip245@gmail.com>
2024-09-28 16:03:40 -0400

    Rubocop fixes for transaction_data_entity
    Fixes rubocop offenses for the transaction_data_entity and its test
    file.

--- backend/app/entities/transaction_data_entity.rb
+++ backend/app/entities/transaction_data_entity.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TransactionDataEntity
   # page_size: number of items to return from the first item after starting_after.
   # starting_after: Token cursor to get records after the transaction_date . transaction ID.
@@ -26,25 +28,34 @@ class TransactionDataEntity
     @users = params[:users]
   end
 
-  def get_data
+  def data
     transactions = serialized_transactions
     transactions.reverse! if @ending_before.present?
 
-    has_next_page = last_possible_record_id != transactions.last[:id] if transactions.any?
-    has_prev_page = first_possible_record_id != transactions.first[:id] if transactions.any?
-
     {
-      meta: {
-        totalCount: Transaction.count,
-        filteredCount: filtered_count,
-        prevPage: has_prev_page ? page_token(transactions.first) : nil,
-        nextPage: has_next_page ? page_token(transactions.last) : nil,
-      },
-      transactions: transactions,
+      meta: meta_data(transactions),
+      transactions: transactions
     }
   end
 
   private
+
+  def meta_data(transactions)
+    {
+      totalCount: Transaction.count,
+      filteredCount: filtered_count,
+      prevPage: previous_page?(transactions) ? page_token(transactions.first) : nil,
+      nextPage: next_page?(transactions) ? page_token(transactions.last) : nil
+    }
+  end
+
+  def previous_page?(transactions)
+    transactions.any? && first_possible_record_id != transactions.first[:id]
+  end
+
+  def next_page?(transactions)
+    transactions.any? && last_possible_record_id != transactions.last[:id]
+  end
 
   def base_transaction_query
     Transaction
@@ -94,13 +105,13 @@ class TransactionDataEntity
   end
 
   def reverse_order(query)
-    reversed_order = @sort_direction == "ASC" ? "DESC" : "ASC"
+    reversed_order = @sort_direction == 'ASC' ? 'DESC' : 'ASC'
     query.reorder("#{@sort_by} #{reversed_order}", id: reversed_order)
   end
 
   def apply_pagination_cursor_after(query)
-    attr_value, id = @starting_after.split(".")
-    if @sort_direction == "ASC"
+    attr_value, id = @starting_after.split('.')
+    if @sort_direction == 'ASC'
       query.where("#{@sort_by} > :attr_value OR \
                 (#{@sort_by} = :attr_value AND transactions.id > :id)",
                   attr_value: attr_value, id: id)
@@ -148,61 +159,59 @@ class TransactionDataEntity
     check_starting_after_valid(params)
     check_ending_before_valid(params)
 
-    if params[:starting_after] && params[:ending_before]
-      raise BadRequestError.new(ending_before: ['Cannot specify both startingAfter and endingBefore'])
-    end
+    return unless params[:starting_after] && params[:ending_before]
+
+    raise BadRequestError.new(ending_before: ['Cannot specify both startingAfter and endingBefore'])
   end
 
   def check_page_size_valid(params)
-    if params[:page_size] && (params[:page_size].to_i < 3 || params[:page_size].to_i > 1000)
-      raise BadRequestError.new(pageSize: ['pageSize must be between 5 and 1000'])
-    end
+    return unless params[:page_size] && (params[:page_size].to_i < 3 || params[:page_size].to_i > 1000)
+
+    raise BadRequestError.new(pageSize: ['pageSize must be between 5 and 1000'])
   end
 
   def check_sort_by_valid(params)
-    if params[:sort_by] && VALID_SORT_BY_ATTRS.exclude?(params[:sort_by])
-      message = "sort_by parameter must be one of (#{VALID_SORT_BY_ATTRS.join(' | ')})"
-      raise BadRequestError.new(sort_by: [message])
-    end
+    return unless params[:sort_by] && VALID_SORT_BY_ATTRS.exclude?(params[:sort_by])
+
+    message = "sort_by parameter must be one of (#{VALID_SORT_BY_ATTRS.join(' | ')})"
+    raise BadRequestError.new(sort_by: [message])
   end
 
   def check_sort_direction_valid(params)
-    if params[:sort_direction] && %w[asc desc].exclude?(params[:sort_direction].downcase)
-      message = "sortDirection must be one of ('asc' | 'desc')"
-      raise BadRequestError.new(sort_direction: [message])
-    end
+    return unless params[:sort_direction] && %w[asc desc].exclude?(params[:sort_direction].downcase)
+
+    message = "sortDirection must be one of ('asc' | 'desc')"
+    raise BadRequestError.new(sort_direction: [message])
   end
 
   def check_starting_after_valid(params)
-    if params[:starting_after] && params[:sort_by] == 'amount'
-      unless page_token_valid?('amount', params[:starting_after])
-        message = "startingAfter has invalid format for sortBy 'amount'"
-        raise BadRequestError.new(starting_after: [message])
-      end
+    return unless params[:starting_after]
+
+    if params[:sort_by] == 'amount' && !page_token_valid?('amount', params[:starting_after])
+      message = "startingAfter has invalid format for sortBy 'amount'"
+      raise BadRequestError.new(starting_after: [message])
     end
 
-    if params[:starting_after] && (params[:sort_by].nil? || params[:sort_by] == 'transaction_date')
-      unless page_token_valid?('transaction', params[:starting_after])
-        message = "startingAfter has invalid format for sortBy 'transaction_date'"
-        raise BadRequestError.new(starting_after: [message])
-      end
-    end
+    return unless (params[:sort_by].nil? || params[:sort_by] == 'transaction_date') &&
+                  !page_token_valid?('transaction', params[:starting_after])
+
+    message = "startingAfter has invalid format for sortBy 'transaction_date'"
+    raise BadRequestError.new(starting_after: [message])
   end
 
   def check_ending_before_valid(params)
-    if params[:ending_before] && params[:sort_by] == 'amount'
-      unless page_token_valid?('amount', params[:ending_before])
-        message = "endingBefore has invalid format for sortBy 'amount'"
-        raise BadRequestError.new(endingBefore: [message])
-      end
+    return unless params[:ending_before]
+
+    if params[:sort_by] == 'amount' && !page_token_valid?('amount', params[:ending_before])
+      message = "endingBefore has invalid format for sortBy 'amount'"
+      raise BadRequestError.new(endingBefore: [message])
     end
 
-    if params[:ending_before] && (params[:sort_by].nil? || params[:sort_by] == 'transaction_date')
-      unless page_token_valid?('transaction', params[:ending_before])
-        message = "endingBefore has invalid format for sortBy 'transaction_date'"
-        raise BadRequestError.new(ending_before: [message])
-      end
-    end
+    return unless (params[:sort_by].nil? || params[:sort_by] == 'transaction_date') &&
+                  !page_token_valid?('transaction', params[:ending_before])
+
+    message = "endingBefore has invalid format for sortBy 'transaction_date'"
+    raise BadRequestError.new(ending_before: [message])
   end
 
   def page_token_valid?(attr, token)
--- backend/test/entities/transaction_data_entity_test.rb
+++ backend/test/entities/transaction_data_entity_test.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class TransactionTest < ActiveSupport::TestCase
-  test 'get_data returns the correct structure' do
-    data = TransactionDataEntity.new.get_data
+class TransactionDataEntityTest < ActiveSupport::TestCase
+  test 'data returns the correct structure' do
+    data = TransactionDataEntity.new.data
     assert data.key?(:meta)
     assert data[:meta].key?(:totalCount)
     assert data[:meta].key?(:filteredCount)
@@ -12,89 +14,89 @@ class TransactionTest < ActiveSupport::TestCase
     assert data[:transactions].is_a?(Array)
   end
 
-  test 'get_data returns transactions sorted by default (transaction_date desc)' do
+  test 'data returns transactions sorted by default (transaction_date desc)' do
     entity = TransactionDataEntity.new
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].each_cons(2).all? { |a, b| a[:transactionDate] >= b[:transactionDate] })
   end
 
-  test 'get_data returns transactions sorted by transaction_date asc' do
+  test 'data returns transactions sorted by transaction_date asc' do
     entity = TransactionDataEntity.new(sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].each_cons(2).all? { |a, b| a[:transactionDate] <= b[:transactionDate] })
   end
 
-  test 'get_data returns transactions sorted by amount asc' do
+  test 'data returns transactions sorted by amount asc' do
     entity = TransactionDataEntity.new(
       sort_by: 'amount',
       sort_direction: 'asc'
     )
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].each_cons(2).all? { |a, b| a[:amount] <= b[:amount] })
   end
 
-  test 'get_data returns transactions sorted by amount desc' do
+  test 'data returns transactions sorted by amount desc' do
     entity = TransactionDataEntity.new(
       sort_by: 'amount',
       sort_direction: 'desc'
     )
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].each_cons(2).all? { |a, b| a[:amount] >= b[:amount] })
   end
 
-  test 'get_data returns filtered transactions by accounts' do
+  test 'data returns filtered transactions by accounts' do
     account_ids = [accounts(:one).id, accounts(:two).id]
     ignored_account_ids = [accounts(:three).id]
     entity = TransactionDataEntity.new(accounts: account_ids)
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].all? { |t| account_ids.include?(t[:account][:id]) })
     assert(data[:transactions].none? { |t| ignored_account_ids.include?(t[:account][:id]) })
   end
 
-  test 'get_data returns filtered transactions by users' do
+  test 'data returns filtered transactions by users' do
     user_ids = [users(:one).id]
     ignored_user_ids = [users(:two).id]
     entity = TransactionDataEntity.new(users: user_ids)
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].all? { |t| user_ids.include?(t[:user][:id]) })
     assert(data[:transactions].none? { |t| ignored_user_ids.include?(t[:user][:id]) })
   end
 
-  test 'get_data returns filtered transactions by search_string for description' do
+  test 'data returns filtered transactions by search_string for description' do
     search_string = 'NATalies'
     entity = TransactionDataEntity.new(search_string: search_string)
-    data = entity.get_data
+    data = entity.data
 
     assert_equal 1, data[:meta][:filteredCount], 'transaction not found by searching description'
     assert(data[:transactions].all? { |t| t[:description].downcase.include?(search_string.downcase) })
   end
 
-  test 'get_data returns filtered transactions by search_string for amount' do
+  test 'data returns filtered transactions by search_string for amount' do
     search_string = '2.3'
     entity = TransactionDataEntity.new(search_string: search_string)
-    data = entity.get_data
+    data = entity.data
 
-    assert(data[:transactions].all? { |t| t[:amount].to_s.include?("2.3") })
+    assert(data[:transactions].all? { |t| t[:amount].to_s.include?('2.3') })
   end
 
-  test 'get_data returns no transactions for overfiltered search_string' do
+  test 'data returns no transactions for overfiltered search_string' do
     search_string = 'abcdegfhijklmnopqrstuvqxyz'
     entity = TransactionDataEntity.new(search_string: search_string)
-    data = entity.get_data
+    data = entity.data
 
     assert_equal 0, data[:meta][:filteredCount]
     assert_equal [], data[:transactions]
   end
 
-  test 'get_data paginates' do
+  test 'data paginates' do
     entity = TransactionDataEntity.new(page_size: 3)
-    data = entity.get_data
+    data = entity.data
     last_transaction = data[:transactions].last
 
     assert_equal 3, data[:transactions].size
@@ -102,127 +104,151 @@ class TransactionTest < ActiveSupport::TestCase
     assert_equal "#{last_transaction[:transactionDate]}.#{last_transaction[:id]}", data[:meta][:nextPage]
   end
 
-  test 'get_data paginates with a starting_after cursor with sort_direction desc with next and prev page' do
+  test 'data paginates with a starting_after cursor with sort_direction desc with next and prev page' do
     # ex: page1: 1/2/24 -> page2: 1/1/24
     # 0 1 2|> 3 4 (5) 6
     transactions = Transaction.order(transaction_date: :desc, id: :desc)
     last_transaction = transactions.offset(2).first
     starting_after = "#{last_transaction.transaction_date}.#{last_transaction.id}"
     entity = TransactionDataEntity.new(page_size: 3, starting_after: starting_after)
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].none? { |t| t[:id] == last_transaction.id })
     assert(data[:transactions].all? { |t| t[:transactionDate] <= last_transaction.transaction_date })
-    assert_equal "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}", data[:meta][:prevPage]
-    assert_equal "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}", data[:meta][:nextPage]
+    assert_equal(
+      "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}",
+      data[:meta][:prevPage]
+    )
+    assert_equal(
+      "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}",
+      data[:meta][:nextPage]
+    )
   end
 
-  test 'get_data paginates with a ending_before cursor with sort_direction desc with next and prev page' do
+  test 'data paginates with a ending_before cursor with sort_direction desc with next and prev page' do
     # ex: page2: 1/1/24 -> page1: 1/2/24
     # 0 (1) 2 3 <|4 5 6
     transactions = Transaction.order(transaction_date: :desc, id: :desc)
     first_transaction = transactions.offset(4).first
     ending_before = "#{first_transaction.transaction_date}.#{first_transaction.id}"
     entity = TransactionDataEntity.new(page_size: 3, ending_before: ending_before)
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].none? { |t| t[:id] == first_transaction.id })
     assert(data[:transactions].all? { |t| t[:transactionDate] >= first_transaction.transaction_date })
-    assert_equal "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}", data[:meta][:prevPage]
-    assert_equal "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}", data[:meta][:nextPage]
+    assert_equal(
+      "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}",
+      data[:meta][:prevPage]
+    )
+    assert_equal(
+      "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}",
+      data[:meta][:nextPage]
+    )
   end
 
-  test 'get_data paginates with a starting_after cursor with sort_direction asc with next and prev page' do
+  test 'data paginates with a starting_after cursor with sort_direction asc with next and prev page' do
     # ex: page1: 1/1/24 -> page2: 1/2/24
     # 0 1 2|> 3 (4) 5 6
     transactions = Transaction.order(transaction_date: :asc, id: :asc)
     last_transaction = transactions.offset(2).first
     starting_after = "#{last_transaction.transaction_date}.#{last_transaction.id}"
     entity = TransactionDataEntity.new(page_size: 3, starting_after: starting_after, sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].none? { |t| t[:id] == last_transaction.id })
     assert(data[:transactions].all? { |t| t[:transactionDate] >= last_transaction.transaction_date })
-    assert_equal "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}", data[:meta][:prevPage]
-    assert_equal "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}", data[:meta][:nextPage]
+    assert_equal(
+      "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}",
+      data[:meta][:prevPage]
+    )
+    assert_equal(
+      "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}",
+      data[:meta][:nextPage]
+    )
   end
 
-  test 'get_data paginates with a ending_before cursor with sort_direction asc' do
+  test 'data paginates with a ending_before cursor with sort_direction asc' do
     # ex: page2: 1/2/24 -> page1: 1/1/24
     # 0 (1) 2 3 <|4 5 6
     transactions = Transaction.order(transaction_date: :asc, id: :asc)
     first_transaction = transactions.offset(4).first
     ending_before = "#{first_transaction.transaction_date}.#{first_transaction.id}"
     entity = TransactionDataEntity.new(page_size: 3, ending_before: ending_before, sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert(data[:transactions].none? { |t| t[:id] == first_transaction.id })
     assert(data[:transactions].all? { |t| t[:transactionDate] <= first_transaction.transaction_date })
-    assert_equal "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}", data[:meta][:prevPage]
-    assert_equal "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}", data[:meta][:nextPage]
+    assert_equal(
+      "#{data[:transactions].first[:transactionDate]}.#{data[:transactions].first[:id]}",
+      data[:meta][:prevPage]
+    )
+    assert_equal(
+      "#{data[:transactions].last[:transactionDate]}.#{data[:transactions].last[:id]}",
+      data[:meta][:nextPage]
+    )
   end
 
-  test 'get_data paginates with a starting_after cursor with no next page with prev page' do
+  test 'data paginates with a starting_after cursor with no next page with prev page' do
     # 0 1 2 3 4|> (5 6)
     transactions = Transaction.order(transaction_date: :desc, id: :desc)
     transactions = transactions.offset(Transaction.count - 2)
     starting_after = "#{transactions.first.transaction_date}.#{transactions.first.id}"
     expected_ending_before = "#{transactions.last.transaction_date}.#{transactions.last.id}"
     entity = TransactionDataEntity.new(page_size: 5, starting_after: starting_after)
-    data = entity.get_data
+    data = entity.data
 
     assert_nil data[:meta][:nextPage]
     assert_equal expected_ending_before, data[:meta][:prevPage]
   end
 
-  test 'get_data paginates with a starting_after cursor with next page with no prev page' do
+  test 'data paginates with a starting_after cursor with next page with no prev page' do
     # |> (0 1 2 3) 4 5 6
     transactions = Transaction.order(transaction_date: :asc, id: :asc)
     transaction = transactions.offset(3).first
     expected_starting_after = "#{transaction.transaction_date}.#{transaction.id}"
     starting_after = '1900-01-01.0' # definitely no records before this.
     entity = TransactionDataEntity.new(page_size: 4, starting_after: starting_after, sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert_equal expected_starting_after, data[:meta][:nextPage]
     assert_nil data[:meta][:prevPage]
   end
 
-  test 'get_data paginates with a ending_before cursor with no next page with prev page' do
+  test 'data paginates with a ending_before cursor with no next page with prev page' do
     # 0 1 2 3 (4 5 6) <|
     transactions = Transaction.order(transaction_date: :asc, id: :asc)
     first_transaction = transactions.offset(Transaction.count - 3).first
     expected_ending_before = "#{first_transaction.transaction_date}.#{first_transaction.id}"
     ending_before = '3000-12-31.999999' # definitely no records after this
     entity = TransactionDataEntity.new(page_size: 3, ending_before: ending_before, sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert_equal expected_ending_before, data[:meta][:prevPage]
     assert_nil data[:meta][:nextPage]
   end
 
-  test 'get_data paginates with a ending_before cursor with next page with no prev page' do
+  test 'data paginates with a ending_before cursor with next page with no prev page' do
     # (0 1 2) <|3 4 5 6
     transactions = Transaction.order(transaction_date: :asc, id: :asc)
     transactions = transactions.offset(2)
     ending_before = "#{transactions[1].transaction_date}.#{transactions[1].id}"
     expected_starting_after = "#{transactions.first.transaction_date}.#{transactions.first.id}"
     entity = TransactionDataEntity.new(page_size: 5, ending_before: ending_before, sort_direction: 'asc')
-    data = entity.get_data
+    data = entity.data
 
     assert_nil data[:meta][:prevPage]
     assert_equal expected_starting_after, data[:meta][:nextPage]
   end
 
-  test 'get_data paginates results with no next or prev page' do
+  test 'data paginates results with no next or prev page' do
     entity = TransactionDataEntity.new(page_size: Transaction.count + 2)
-    data = entity.get_data
+    data = entity.data
 
     assert_nil data[:meta][:prevPage]
     assert_nil data[:meta][:nextPage]
   end
 
-  test 'get_data returns the correct filtered_items count' do
+  test 'data returns the correct filtered_items count' do
     user_ids = [users(:one).id]
     count = Transaction
             .includes(:account, { account: :user })
@@ -230,7 +256,7 @@ class TransactionTest < ActiveSupport::TestCase
             .where(accounts: { user_id: user_ids })
             .count
     entity = TransactionDataEntity.new(users: user_ids)
-    data = entity.get_data
+    data = entity.data
 
     assert_equal count, data[:meta][:filteredCount]
   end
