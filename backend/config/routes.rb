d359bbfa400856345049ab56bc94bfdd3cb4bf46
Philip Dayboch <philip245@gmail.com>
2024-10-21 22:50:32 -0400

    Add new trends controller
    This adds a new controller for returning trends data. The only
    endpoint we have implemented so far is over_time, which returns
    data in a time series. The endpoint sends the request straight
    through to the transaction trend over time entity. The only
    validation it will perform is on the date params to ensure
    start_date and end_date are present.
    
    This also makes a small update to the routes file to move the
    supported_accounts endpoint as a collection under the accounts
    resource.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   resources :transactions, only: %i[index create update destroy]
-  resources :accounts, only: %i[index create update destroy]
+  resources :accounts, only: %i[index create update destroy] do
+    collection do
+      get :supported, to: 'supported_accounts#index'
+    end
+  end
   resources :users, only: %i[index create update destroy]
   resources :subcategories, only: %i[create update destroy]
   resources :categories, only: %i[index create update destroy]
@@ -12,12 +16,9 @@ Rails.application.routes.draw do
     resources :rules, only: %i[index create update destroy]
   end
 
-  get 'accounts/supported', to: 'supported_accounts#index'
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :trends, only: [] do
+    collection do
+      get 'overTime', to: 'trends#over_time'
+    end
+  end
 end
