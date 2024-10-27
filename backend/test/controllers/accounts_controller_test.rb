7782ef64d7e0d9e25701780f6637fad834dc63f3
Philip Dayboch <philip245@gmail.com>
2024-10-27 14:25:40 -0400

    Improve account controller test
    This improvees the account controller test by removing all rubocop
    offenses and adding a new test to ensure that the controller is
    calling the AccountDataEntity to fetch the Account index data.

--- backend/test/controllers/accounts_controller_test.rb
+++ backend/test/controllers/accounts_controller_test.rb
@@ -1,20 +1,28 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class AccountsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @account = accounts(:one)
-  end
-
-  test "should get index" do
-    get accounts_url, as: :json
+  test 'should get index' do
+    get accounts_url
     assert_response :success
   end
 
-  test "should create account" do
+  test 'should get index from AccountDataEntity' do
+    original_new = AccountDataEntity.method(:new)
+
+    AccountDataEntity.expects(:new).returns(original_new.call)
+
+    get accounts_url
+  end
+
+  test 'should create account' do
+    account = accounts(:one)
+
     assert_difference('Account.count') do
       post accounts_url, params: {
         accountProvider: 'ChaseFreedomCreditCard',
-        userId: @account.user_id,
+        userId: account.user_id,
         statementDirectory: 'credit cards/chase'
       }
     end
@@ -22,41 +30,49 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_response :created
   end
 
-  test "should return error with invalid accountProvider create account" do
+  test 'should return error with invalid accountProvider create account' do
+    account = accounts(:one)
+
     post accounts_url, params: {
       accountProvider: 'NonExistantProvider',
-      userId: @account.user_id,
+      userId: account.user_id,
       statementDirectory: 'credit cards/chase'
     }
 
     assert_response :unprocessable_entity
-    json_response = JSON.parse(response.body)
+    json_response = response.parsed_body
 
     expected_error = {
       'field' => 'accountProvider',
-      'message' =>  "accountProvider 'NonExistantProvider' is not a valid value."
+      'message' => "accountProvider 'NonExistantProvider' is not a valid value."
     }
 
     assert_includes json_response['errors'], expected_error
   end
 
-  test "should update account" do
-    patch account_url(@account), params: {
+  test 'should update account' do
+    account = accounts(:one)
+
+    patch account_url(account), params: {
       active: false,
       statementDirectory: 'credit cards/new'
     }
 
     assert_response :success
-    @account.reload
-    assert_equal @account.active, false, 'Account was not updated'
-    assert_equal @account.statement_directory, 'credit cards/new', 'Account was not updated'
+    account.reload
+    assert_equal account.active, false, 'Account was not updated'
+    assert_equal account.statement_directory, 'credit cards/new', 'Account was not updated'
   end
 
-  test "should return error when updating account with disallowed params" do
-    patch account_url(@account), params: { accountProvider: 'BarclaysViewCreditCard' }
+  test 'should return error when updating account with disallowed params' do
+    account = accounts(:one)
+
+    patch account_url(account), params: {
+      accountProvider: 'BarclaysViewCreditCard'
+    }
 
     assert_response :unprocessable_entity
-    json_response = JSON.parse(response.body)
+    json_response = response.parsed_body
 
     expected_error = {
       'field' => 'accountProvider',
@@ -66,9 +82,11 @@ class AccountsControllerTest < ActionDispatch::IntegrationTest
     assert_includes json_response['errors'], expected_error
   end
 
-  test "should destroy account" do
+  test 'should destroy account' do
+    account = accounts(:one)
+
     assert_difference('Account.count', -1) do
-      delete account_url(@account), as: :json
+      delete account_url(account)
     end
 
     assert_response :no_content
