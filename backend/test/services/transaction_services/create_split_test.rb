e2d5fd794b8072c625b54f1d4db74631adcfae71
Philip Dayboch <philip245@gmail.com>
2024-11-05 15:33:42 -0500

    Add CreateSplit service
    This adds a new Create Split service which is responsible for
    creating transaction splits. The new transactions will be attached
    to the original transactions through the .splits association. This
    also sets the has_splits flag on the original transaction upon
    successful split.
    
    The service validates the split params to ensure all splits have
    the same amount sign (postive/negative) as the original
    transaction. We also ensure that the absolute value of the sum of
    the split amounts does not exceed the absolute value of the
    original transaction amount.
    
    This service is not yet used, but will be used in an upcoming
    Transactions Split Controller.

---
+++ backend/test/services/transaction_services/create_split_test.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module TransactionServices
+  class CreateSplitTest < ActiveSupport::TestCase
+    test 'successful split returns the correct response' do
+      original = transactions(:four)
+      subcategory = subcategories(:restaurant)
+      split_params = [
+        { amount: 40.0, description: 'Split 1', subcategory_id: subcategory.id },
+        { amount: 30.0, description: 'Split 2', subcategory_id: subcategory.id }
+      ]
+
+      service = CreateSplit.new(original, split_params)
+      result = service.call
+      splits = original.reload.splits
+
+      assert_includes result, :original
+      assert_includes result, :splits
+      assert_equal 2, result[:splits].count
+
+      assert_equal TransactionSerializer.new(original).as_json, result[:original]
+
+      serialized_splits = splits.map { |s| TransactionSerializer.new(s).as_json }
+      assert_equal(serialized_splits.sort_by { |s| s[:id] }, result[:splits].sort_by { |s| s[:id] })
+    end
+
+    test 'successful split sets the has_splits flag on original' do
+      original = transactions(:four)
+      split_params = [
+        { amount: 100.0, description: 'Split', subcategory_id: original.subcategory_id }
+      ]
+
+      assert_not original.has_splits
+      service = CreateSplit.new(original, split_params)
+      result = service.call
+
+      assert original.has_splits
+      assert(result[:splits].all? { |s| s[:splitFromId] == original.id })
+    end
+
+    test 'successful split adjusts the original amount correctly' do
+      original = transactions(:four)
+      original_amount = original.amount
+      split_params = [
+        { amount: 200.0, description: 'Split 1' },
+        { amount: 100.0, description: 'Split 2' }
+      ]
+
+      service = CreateSplit.new(original, split_params)
+      service.call
+
+      expected_remaining_amount = original_amount.to_d - split_params.sum { |s| s[:amount].to_d }
+      assert_equal expected_remaining_amount, original.reload.amount
+    end
+
+    test 'sets transaction_date to the original transaction when not specified' do
+      original = transactions(:four)
+      original_date = original.transaction_date
+
+      split_params = [
+        { amount: 50.0, description: 'Split without date', subcategory_id: original.subcategory_id }
+      ]
+
+      service = CreateSplit.new(original, split_params)
+      service.call
+
+      created_split = original.splits.last
+      assert_equal original_date, created_split.transaction_date
+    end
+
+    test 'raises error if split amounts do not match original transaction sign' do
+      original = transactions(:four)
+      split_amount = (original.amount.to_d / 2) - 1
+
+      split_params = [
+        { amount: split_amount, description: 'Split 1' },
+        { amount: -split_amount, description: 'Split 2' }
+      ]
+
+      service = CreateSplit.new(original, split_params)
+
+      assert_raises CreateSplit::SplitAmountSignMismatchError do
+        service.call
+      end
+    end
+
+    test 'raises error if split amounts exceed original transaction amount' do
+      original = transactions(:four)
+      excess_amount = (original.amount.to_d / 2) + 1
+      split_params = [
+        { amount: excess_amount, description: 'Split 1' },
+        { amount: excess_amount, description: 'Split 2' }
+      ]
+
+      service = CreateSplit.new(original, split_params)
+
+      assert_raises CreateSplit::SplitAmountExceedsOriginalError do
+        service.call
+      end
+    end
+
+    test 'raises error if original transaction is nil' do
+      split_params = [{ amount: 50.0, description: 'Split', subcategory_id: 1 }]
+
+      assert_raises(ArgumentError, 'Original transaction cannot be nil') do
+        TransactionServices::CreateSplit.new(nil, split_params)
+      end
+    end
+  end
+end
