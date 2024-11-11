94b9c086e143a4ec485901404b43f64e310076a1
Philip Dayboch <philip245@gmail.com>
2024-11-11 12:42:42 -0500

    Split frontend app into separate repository
    This removes all frontend files to split out the frontend app into
    a separate repository. This repository will remain the backend app.
    In a followup commit, we will split out the nginx code into a new
    repo, as well as the docker-compose into into a new deploy repo.

--- frontend/src/app/transactions/helpers.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-
-export const amountClass = (amount: number): string => {
-  return clsx({
-    'text-green-600': amount >= 0,
-    'text-red-600': amount < 0,
-  })
-};
--- frontend/src/utils/api-utils.ts
+++
@@ -1,3 +0,0 @@
-export const getBaseApiUrl = () => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:3005';
-};
